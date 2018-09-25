ad27c046bfc57206662ac7e2e298a223afafc8ba
Niraj Yadav <neerajy65@gmail.com>
2018-09-25 06:34:09 +0530

    input done

--- Solution.py
+++ Solution.py
@@ -8,35 +8,45 @@ Assignment 1
 -> Niraj Yadav (B16024)
 """
 from graph import Graph
+import numpy as np
 # import os
 # os.chdir("TestCases/")
 # filenames = ["euc_100", "euc_250", "euc_500", "noneuc_100", "noneuc_250", "noneuc_500"]
 
-# f=open("euc_100", "r")
+f=open("euc_100", "r")
+eu = f.readline().split()[0]
+print (eu)
 
-
-
-eu = input()
 if eu=="euclidean":
     euclid = True
 else:
     euclid = False
+print euclid
+n = f.readline()
+n=int(n)
+print n
+#g = Graph(n)
 
-n = int(input())
-
-g = Graph(n)
-
-
+i=0
 coordinates = []
-for _ in range(n):
-    x = [float(i) for i in input().split()]
+for i in range(n):
+    x = [f.readline().split()]
+    x = [float(i) for i in x[0]]
     coordinates.append(x)
-
+    
+i=0
 distances = []
-for _ in range(n):
-    x = [float(i) for i in input().split()]
+for i in range(n):
+    x = [f.readline().split()]
+    x = [float(i) for i in x[0]]
     distances.append(x)
 
-g.graph = distances
+print(coordinates)
+print(distances)
 
-g.primMST()
+
+#g.graph = distances
+
+#g.primMST()
+
+
